c74bec1c6145cd30dc592ce9bdcf106008feaa47
ilyani04 <nurnilyani@gmail.com>
2024-06-07 02:35:10 +0800

    updatedListOS

---
+++ listOnlineSession.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icon.png"/>
+    <title>Online Session</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <?php include('nav-T.php'); ?>
+    <main>
+    <div class="container my-5">
+        <h2>List of Online Session</h2>
+        <a class="btn btn-secondary" href="addOnlineSession.php" role="button">Add Online Session</a>
+        <br>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ONLINE SESSION ID</th>
+                    <th>COURSE CODE</th>
+                    <th>CONTENT</th>
+                    <th>OPTIONS</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $database = "selfodb";
+
+                //create connection
+                $connection = new mysqli($servername, $username, $password, $database);
+
+                //check connection
+                if ($connection->connect_error){
+                    die("Connection failed: ". $connection->connect_error);
+                }
+
+                //read all row from database table
+                $sql = "SELECT * FROM online_session";
+                $result = $connection->query($sql);
+
+                if (!$result){
+                    die("Invalid query: ". $connection->error);
+                }
+
+                //read data of each row
+                while($row = $result->fetch_assoc()){
+                    echo "<tr>
+                    <td>$row[online_id]</td>
+                    <td>$row[course_code]</th>
+                    <td>$row[link_meet]</th>
+                    <td>
+                        <a class='btn btn-primary btn-sm' href='updateOnlineSession.php?online_id=$row[online_id]'>Update</a>
+                        <a class='btn btn-danger btn-sm' href='deleteOnlineSession.php?online_id=$row[online_id]'>Delete</a>
+                    </td>
+                </tr>
+                ";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    </main>
+</body>
+</html>
--- updateOnlineSession.php
+++ updateOnlineSession.php
@@ -10,53 +10,46 @@ $database = "selfodb";
 // Database connection
 $conn = new mysqli($servername, $username, $password, $database);
 
-$tutor_id ="";
-$tutor_password ="";
-$tutor_name ="";
-$tutor_phone ="";
-$tutor_email ="";
+$online_id ="";
+$course_code ="";
+$link_meet ="";
 
 $errorMessage = "";
 $successMessage = "";
 
 try {
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        if (!isset($_GET["tutor_id"])) {
-            header("location: listTutor.php");
+        if (!isset($_GET["online_id"])) {
+            header("location: listOnlineSession.php");
             exit;
         }
 
-        $tutor_id = $_GET["tutor_id"];
+        $online_id = $_GET["online_id"];
 
-        $sql = "SELECT * FROM tutor WHERE tutor_id = ?";
+        $sql = "SELECT * FROM online_session WHERE online_id = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("s", $tutor_id);
+        $stmt->bind_param("s", $online_id);
         $stmt->execute();
         $result = $stmt->get_result();
         $row = $result->fetch_assoc();
 
         if (!$row) {
-            header("location: listTutor.php");
+            header("location: listOnlineSession.php");
             exit;
         }
 
-        $tutor_password = $row["tutor_password"];
-        $tutor_name = $row["tutor_name"];
-        $tutor_phone = $row["tutor_phone"];
-        $tutor_email = $row["tutor_email"];
+        $course_code = $row["course_code"];
+        $link_meet = $row["link_meet"];
     } elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Retrieve and sanitize user input
-        $tutor_id = $_POST['tutor_id'];
-        $tutor_password = $_POST['tutor_password'];
-        $tutor_name = $_POST['tutor_name'];
-        $tutor_phone = $_POST['tutor_phone'];
-        $tutor_email = $_POST['tutor_email'];
-
+        $online_id = $_POST['online_id'];
+        $course_code = $_POST['course_code'];
+        $link_meet = $_POST['link_meet'];
 
         // Prepare an update statement
-        $sql = "UPDATE tutor SET tutor_password = ?, tutor_name = ?, tutor_phone = ?, tutor_email = ?  WHERE tutor_id = ?";
+        $sql = "UPDATE online_session SET course_code = ?, link_meet = ?  WHERE online_id = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sssss", $tutor_password, $tutor_name, $tutor_phone, $tutor_email, $tutor_id);
+        $stmt->bind_param("sss", $course_code, $link_meet,  $online_id);
 
         // Execute the statement
         $stmt->execute();
@@ -65,7 +58,7 @@ try {
         if ($stmt->affected_rows > 0) {
             $successMessage = "Record updated successfully.";
             // Redirect to listTutor.php after successful update
-            header("Location: /SLMS2/listTutor.php");
+            header("Location:listOnlineSession.php");
             exit();
         } else {
             $errorMessage = "No record found with the specified ID.";
@@ -88,14 +81,14 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="images/icon.png"/>
-    <title>Tutor</title>
+    <title>Online Session</title>
     <link rel="stylesheet" href="css/style2.css">
     <script src ="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <main>
     <div class="container my-5">
-        <h2>Update Tutor</h2>
+        <h2>Update Online Session</h2>
 
         <?php
         if (!empty($errorMessage)) {
@@ -121,31 +114,19 @@ try {
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label">ID</label>
                 <div class="col-sm-6">
-                    <input type="text" class="form-control" name="tutor_id" value="<?php echo $tutor_id; ?>" readonly>
+                    <input type="text" class="form-control" name="online_id" value="<?php echo $online_id; ?>" readonly>
                 </div>
             </div>
             <div class="row mb-3">
-                <label class="col-sm-3 col-form-label">Password</label>
+                <label class="col-sm-3 col-form-label">Course Code</label>
                 <div class="col-sm-6">
-                    <input type="text" class="form-control" name="tutor_password" value="<?php echo $tutor_password; ?>">
+                    <input type="text" class="form-control" name="course_code" value="<?php echo $course_code; ?>">
                 </div>
             </div>
             <div class="row mb-3">
-                <label class="col-sm-3 col-form-label">Name</label>
+                <label class="col-sm-3 col-form-label">Link Meeting</label>
                 <div class="col-sm-6">
-                    <input type="text" class="form-control" name="tutor_name" value="<?php echo $tutor_name; ?>">
-                </div>
-            </div>
-            <div class="row mb-3">
-                <label class="col-sm-3 col-form-label">Phone Number</label>
-                <div class="col-sm-6">
-                    <input type="text" class="form-control" name="tutor_phone" value="<?php echo $tutor_phone; ?>">
-                </div>
-            </div>
-            <div class="row mb-3">
-                <label class="col-sm-3 col-form-label">Email</label>
-                <div class="col-sm-6">
-                    <input type="text" class="form-control" name="tutor_email" value="<?php echo $tutor_email; ?>">
+                    <input type="text" class="form-control" name="link_meet" value="<?php echo $link_meet; ?>">
                 </div>
             </div>
             <div class="row mb-3">
@@ -153,7 +134,7 @@ try {
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
                 <div class="col-sm-3 d-grid">
-                    <a class="btn btn-outline-primary" href="listTutor.php" role="button">Cancel</a>
+                    <a class="btn btn-outline-primary" href="listOnlineSession.php" role="button">Cancel</a>
                 </div>
             </div>
         </form>
